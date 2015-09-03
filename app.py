c506f0f4d5cc7e90dcd862456fdc6475589b458b
Aniket Aranake <aniketaranake@gmail.com>
2015-09-03 16:00:16 -0400

    Cleanup of git repo and small modification to app.py

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 from flask import Flask, render_template, request, redirect
+from aranake_stock_plotter import getstockdata,generateplot
+import traceback
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def main():
+  print "Arrived in main()"
   return redirect('/index')
 
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+
+  print "Arrived in index()"
+  print "request.method: ", request.method
+  # Show the initial question sheet
+  if request.method=='GET':
+    print "Arrived in GET"
+    return render_template('layout.html',bokeh_script="",bokeh_div="",note="")
+
+  # Form was filled out, time to draw the plot
+  else:
+    print "Arrived in POST"
+
+    try:
+        # Call quandl API to get stock data
+        data = getstockdata(request.form['stock'])
+    except:
+        note = "ERROR getting stock data! Faulty ticker symbol?"#+traceback.format_exc()
+        return render_template('layout.html',bokeh_script="",bokeh_div="",note=note)
+
+    # Generate bokeh plot
+    desired_columns = request.form.getlist('features')
+    script,div,note = generateplot(data,desired_columns,request.form['stock'])
+
+    # Render
+    return render_template('layout.html',bokeh_script=script,bokeh_div=div,note=note)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(host='0.0.0.0',port=33507,debug=True)
